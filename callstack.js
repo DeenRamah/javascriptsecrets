e6a144f1fca4ccdca2283069b2ddca10aaef3c5f
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-08-17 20:50:54 +0300

    Create callstack.js

---
+++ callstack.js
@@ -0,0 +1,11 @@
+//call stack keeps track of the currently executing code
+function deen(){
+  console.log("Deen Dev");
+}
+
+function taliah(){
+  console.log("Taliah Dev");
+}
+
+deen();
+taliah();
