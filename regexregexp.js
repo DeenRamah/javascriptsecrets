4e559b569608541b3d2225815d1ca2bf31beb189
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-08-18 17:42:33 +0300

    Create regexregexp.js

---
+++ regexregexp.js
@@ -0,0 +1,5 @@
+//you can create a regular expression using either a regex literal or the RegExp constructor
+const regex1 =/pattern/;
+
+//using the RegExp constructor
+const regex2 = new RegExp('pattern');
