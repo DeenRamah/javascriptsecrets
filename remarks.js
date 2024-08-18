7783562d47a8ca8f2bf18e01d8bfa8468f5f9cb3
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-08-18 16:37:53 +0300

    Create remarks.js

---
+++ remarks.js
@@ -0,0 +1,21 @@
+//when writing vanillla js code and you can use shortcuts like in the examples below. if for example you are working on react app 
+//for location and history you may want to use hooks provided by react dom
+window.document.getElementById('root');
+//or
+document.getElementById('root');
+//or
+window.location.reload()
+//or
+location.reload()
+
+window.localStorage.setItem('name', 'Deen Dev');
+//or
+localStorage.setItem('name', 'Deen Dev');
+//or
+window.history.back()
+//or
+history.back()
+
+//in react
+const history = useNavigate()
+const location = useLocation()
