2d0d56b55dfd6b769dd029917085e0b33a94b8d1
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-08-18 17:52:06 +0300

    Create replacingtxt.js

---
+++ replacingtxt.js
@@ -0,0 +1,7 @@
+//the replace methods alllows you to replace text that matches a pattern
+
+const text = "Hello world";
+const pattern = /Hello/;
+
+const newText = text.replace(pattern, "Hi");
+console.log(newText);
