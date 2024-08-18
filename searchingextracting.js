278776531de17f925b2936cf106ba79154ecfa94
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-08-18 17:49:12 +0300

    Create searchingextracting.js

---
+++ searchingextracting.js
@@ -0,0 +1,5 @@
+const text ="Am the best ever the most violent the most erronous and most complete athlete of all time";
+const pattern = /[a-zA-Z]+/g; //matching words
+
+const matches = text.match(pattern);
+console.log(matches)
