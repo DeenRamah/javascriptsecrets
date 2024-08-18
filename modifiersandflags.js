239ed10a7187b9d34a1e6a65b30fa9eaf5fdcc17
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-08-18 17:54:26 +0300

    Create modifiersandflags.js

---
+++ modifiersandflags.js
@@ -0,0 +1,4 @@
+//you can also add modifiers (also called flags to regex patterns to modify their behavior)
+//common flags include i (case s=isensitive) and g(global to find all matches)
+
+cont pattern = /apples/gi;
