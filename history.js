211683410c45f5246de029277c5ab9fd933138a5
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-08-18 16:23:13 +0300

    Create history.js

---
+++ history.js
@@ -0,0 +1,9 @@
+//returns an history object which contains the pages visited by a user
+setTimeout(()=>{
+  window.history.go(-1);
+} 2000)
+
+//or
+setTimeout(()=>{
+  window.history.back()
+}, 2000)
