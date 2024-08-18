c30abd21f3326130a82acca4eb3120ad19ea3f2e
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-08-18 16:25:39 +0300

    Create document.js

---
+++ document.js
@@ -0,0 +1,4 @@
+//returns the document object as an html document loaded into a web browser
+console.log(window.document.title);
+//getting the title of the page
+window.document.body.innerHTML = "Hello world";
